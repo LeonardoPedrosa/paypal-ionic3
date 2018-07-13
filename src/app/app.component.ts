bc6ab09dc4615648ac31853370268417a601d1c3
Leonardo Pedrosa <leonardopedrosa@zaite15.local>
2018-07-13 09:39:44 -0300

    melhoria visual em checar dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,29 +11,29 @@ import { TabsPage } from '../pages/tabs/tabs';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = SplashPage;
+  rootPage:any = ProdutosPage;
 
-  // constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-  //   platform.ready().then(() => {
-  //     // Okay, so the platform is ready and our plugins are available.
-  //     // Here you can do any higher level native things you might need.
-  //     statusBar.styleDefault();
-  //     splashScreen.hide();
-  //   });
-  // }
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
- 
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-
-        statusBar.styleDefault();
-
-        let splash = modalCtrl.create(SplashPage);
-        splash.present();
-
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
-
   }
+
+  // constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
+ 
+  //   platform.ready().then(() => {
+
+  //       statusBar.styleDefault();
+
+  //       let splash = modalCtrl.create(SplashPage);
+  //       splash.present();
+
+  //   });
+
+  // }
 }
   
 
